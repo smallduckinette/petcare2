99ecfb85842ab9730c72302a5eaf753e14a490ae
small_duck <small_duck@technoturtle.net>
2026-04-11 01:28:16 +0100

    SFML basic app

--- petcare.cpp
+++ petcare.cpp
@@ -1,50 +1,29 @@
 #include <memory>
 #include <fmt/core.h>
 
-#include <SDL.h>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
   fmt::print("Starting Petcare\n");
 
-  if (SDL_Init(SDL_INIT_VIDEO) < 0)
+  sf::RenderWindow window(sf::VideoMode(800, 600), "BricABrac");
+  window.setVerticalSyncEnabled(true);
+
+  while (window.isOpen())
   {
-    throw std::runtime_error(fmt::format("Failed to init video: {}", SDL_GetError()));
-  }
-
-  std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> window(
-      SDL_CreateWindow("Petcare", SDL_WINDOWPOS_UNDEFINED,
-                       SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN),
-      SDL_DestroyWindow);
-
-  if (!window)
-  {
-    throw std::runtime_error(fmt::format("Failed to create window: {}", SDL_GetError()));
-  }
-
-  std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)> renderer(
-      SDL_CreateRenderer(window.get(), -1, SDL_RENDERER_ACCELERATED),
-      SDL_DestroyRenderer);
-
-  if (!renderer)
-  {
-    throw std::runtime_error(fmt::format("Failed to create renderer: {}", SDL_GetError()));
-  }
-
-  bool running = true;
-  while (running)
-  {
-    SDL_Event event;
-    while(SDL_PollEvent(&event))
+    sf::Event event;
+    while (window.pollEvent(event))
     {
-      if(event.type == SDL_QUIT)
+      if(event.type == sf::Event::Closed)
       {
-        running = false;
+        window.close();
       }
     }
-
-    SDL_RenderClear(renderer.get());
-    SDL_RenderPresent(renderer.get());
+    window.clear();
+    window.display();
   }
 
   fmt::print("Closing\n");
