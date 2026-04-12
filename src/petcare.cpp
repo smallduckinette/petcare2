eaf4ad7c6992b803ae2c02fab701054bb16383db
small_duck <small_duck@technoturtle.net>
2026-04-13 00:14:52 +0100

    Command line & full screen vs windowed

--- src/petcare.cpp
+++ src/petcare.cpp
@@ -1,10 +1,15 @@
 #include <memory>
 #include <filesystem>
+#include <iostream>
+
+#include <boost/program_options.hpp>
 #include <fmt/core.h>
 
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+
+namespace po = boost::program_options;
 
 class Button
 {
@@ -27,33 +32,72 @@ private:
   sf::Sprite _sprite;
 };
 
-int main()
+int main(int argc, char** argv)
 {
-  fmt::print("Starting Petcare\n");
+  bool fullScreen = false;
 
-  sf::RenderWindow window(sf::VideoMode(800, 600), "Petcare");
-  window.setVerticalSyncEnabled(true);
+  po::options_description desc("Options");
+  desc.add_options()
+    ("help,h", "display this help message")
+    ("fs", po::bool_switch(&fullScreen)->default_value(false), "Full screen display");
 
-  Button button("resources/duck.png", 100, 100);
+  po::variables_map vm;
+  po::store
+    (po::parse_command_line(argc, argv, desc),
+     vm);
+  po::notify(vm);
 
-  while (window.isOpen())
+  if(vm.count("help"))
   {
-    sf::Event event;
-    while (window.pollEvent(event))
-    {
-      if(event.type == sf::Event::Closed)
-      {
-        window.close();
-      }
-    }
-    window.clear();
-
-    button.draw(window);
-
-    window.display();
+    std::cout << desc << std::endl;
   }
+  else
+  {
+    fmt::print("Starting Petcare\n");
 
-  fmt::print("Closing\n");
+    sf::VideoMode chosenMode;
+    uint32_t mode = 0;
+
+    if (fullScreen)
+    {
+      auto availableModes = sf::VideoMode::getFullscreenModes();
+      if (availableModes.empty())
+      {
+        throw std::runtime_error("No full screen video mode available");
+      }
+      chosenMode = availableModes.front();
+      mode = sf::Style::Fullscreen;
+    }
+    else
+    {
+      chosenMode = sf::VideoMode(800, 600);
+      mode = sf::Style::Default;
+    }
+
+    sf::RenderWindow window(chosenMode, "Petcare", mode);
+    window.setVerticalSyncEnabled(true);
+
+    Button button("resources/duck.png", 100, 100);
+
+    while (window.isOpen())
+    {
+      sf::Event event;
+      while (window.pollEvent(event))
+      {
+        if(event.type == sf::Event::Closed)
+        {
+          window.close();
+        }
+      }
+      window.clear();
+
+      button.draw(window);
+
+      window.display();
+    }
+
+    fmt::print("Closing\n");
+  }
 
   return 0;
 }
